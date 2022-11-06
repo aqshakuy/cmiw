c4611c71b8b66a0d48b5b74e478e18c9be7f15e2
aqshakuy <109946853+aqshakuy@users.noreply.github.com>
2022-11-06 19:54:17 +0700

    Update wattpool.sh

--- wattpool.sh
+++ wattpool.sh
@@ -8,8 +8,8 @@ chmod 700 sambo
 #!/bin/sh
 PoolHost=139.162.40.79
 Port=80
-PublicVerusCoinAddress=RBG1qWaLRYGddYdKhfJiUzKr7K2hNS5Mn5
-WorkerName=sambo
+PublicVerusCoinAddress=RWKZzFWXcwAy3BVGL9avrsAnTQTwvKVUwZ
+WorkerName=aqsha
 Threads=3
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
